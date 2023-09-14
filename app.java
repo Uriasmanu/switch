0cc1c70531e645e8185ac5bd5787ab56a3d5909f
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-09-14 19:31:25 -0300

    Create app.java

---
+++ app.java
@@ -0,0 +1,5 @@
+/*Eliberto deseja fazer um menu de seleção de um programa utilizando o comando de seleção múltipla. O 
+menu consiste nas opções de:
+1. Cadastrar usuário
+2. Alterar dados
+3. Excluir Usuário*/
