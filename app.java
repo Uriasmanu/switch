c16ca6b33736e2d23af2ae9a226c7620c0418a77
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-09-14 19:38:40 -0300

    .

--- app.java
+++ app.java
@@ -3,3 +3,17 @@ menu consiste nas opções de:
 1. Cadastrar usuário
 2. Alterar dados
 3. Excluir Usuário*/
+
+@impot javax.swing.JOptionPane;
+
+public class switchCase {
+    public static void main (String[] args) {
+        int opcaos;
+
+        JOptionPane.showMessageDialog("Escolha uma opção : ");
+        opcaos = Integer.parseInt(JOptionPane.showMessageDialog("1. Cadastrar usuário /n
+        2. Alterar dados /n
+        3. Excluir Usuário");
+        
+    }
+}
